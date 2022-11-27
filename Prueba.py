b6a7694fa0fc41827e6055a5afdd325cf1d66176
OscarFlorin <oscar.florinc@icloud.com>
2022-11-26 18:04:31 -0600

    Se hizo una prueba para sincronizar mi carpeta

--- Prueba.py
+++ Prueba.py
@@ -1,3 +1,11 @@
-#Este es mi primera prueba para Python en github
+#Este es mi primera prueba para Python en github.
 
 print("Hola mundo")
+
+#Este es mi otra prueba
+numero_1= int(input("Ingresa el primer numero positivo: "))
+numero_2= int(input("Ingresa el segundo numero positivo: "))
+
+
+suma = numero_1+numero_2
+print(suma)
