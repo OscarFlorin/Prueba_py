4a2c47b39457605cc74f793335a3458694038f8b
OscarFlorin <oscar.florinc@icloud.com>
2022-11-26 18:14:18 -0600

    Actualizacion del archivo

--- Prueba.py
+++ Prueba.py
@@ -9,3 +9,12 @@ numero_2= int(input("Ingresa el segundo numero positivo: "))
 
 suma = numero_1+numero_2
 print(suma)
+
+""" 
+Estos son los comandos para poder sincronizar archivos a GitHub
+$ git add . #Este comando sube todas las modificaciones 
+$ git commit -m "Ejamplo de mensaje para commit" # Este comando captura una snapshot de los cambios preparados en este momento
+$ git push #Este comando sincroniza el archivo a github """
+
+
+
